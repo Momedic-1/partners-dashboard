932df35115eab521d89a1c78c3fb79cb26ae02aa
balogun14 <awwalbalogun06@gmail.com>
2025-05-11 00:36:24 +0100

    remove linting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // This disables running ESLint during the build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
